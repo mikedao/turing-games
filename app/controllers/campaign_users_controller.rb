9b8ea04d530a851b5bc8514bacd774e52156ef9a
Michael Dao <mike@turing.edu>
2023-06-04 15:56:02 +0000

    hide buttons as apporpriate

--- app/controllers/campaign_users_controller.rb
+++ app/controllers/campaign_users_controller.rb
@@ -1,8 +1,6 @@
 class CampaignUsersController < ApplicationController
   def create
-    binding.pry
     CampaignUser.create(campaign_id: params[:campaign], user_id: params[:user])
-    binding.pry
     redirect_to campaign_path(params[:campaign])
   end
 
--- spec/features/campaign-users/user_can_remove_themselves_from_a_campaign_spec.rb
+++ spec/features/campaign-users/user_can_remove_themselves_from_a_campaign_spec.rb
@@ -32,5 +32,27 @@ RSpec.describe 'User can remove themselves from a campaign', type: :feature do
         expect(page).to_not have_content(user.username)
       end
     end
+
+    it 'can not remove themselves from a campaign they are not a part of' do
+      user = User.create( username: 'username_test',
+                          first_name: 'first_name_test',
+                          last_name: 'last_name_test',
+                          email: 'email_test',
+                          password: 'password_test',
+                          role: 0)
+      campaign = Campaign.create( title: 'campaign_1',
+                                  description: 'campaign_1_description'
+                                )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit campaign_path(campaign)
+
+      within('.campaign-users') do
+        expect(page).to_not have_content(user.username)
+      end
+
+      expect(page).to_not have_link('Leave Campaign')
+    end
   end
 end
