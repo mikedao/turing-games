1f84738f514358dfacf4233310fe3d8f5acb870c
Michael Dao <mike@turing.edu>
2023-06-05 14:29:56 -0600

    dynamically remove image from edit form

--- app/controllers/admin/campaigns/image_controller.rb
+++ app/controllers/admin/campaigns/image_controller.rb
@@ -1,8 +1,13 @@
 class Admin::Campaigns::ImageController < Admin::BaseController
+  include ActionView::RecordIdentifier
 
   def destroy
     campaign = Campaign.find(params[:campaign_id])
     campaign.image.purge_later
-    redirect_to edit_admin_campaign_path(campaign), notice: "Image successfully removed."
+
+    respond_to do |format|
+      format.html { redirect_to edit_admin_campaign_path(campaign), notice: "Image successfully removed." }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(campaign, :image)) }
+    end
   end
 end
